890c5d9b83b2539a78f1b5030e683314c6bdb1f3
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-11-03 15:26:32 -0800

    Building minimum skeleton

--- src/main/java/org/jenkinsci/plugins/pipelineeditor/WorkflowVisualEditor.java
+++ src/main/java/org/jenkinsci/plugins/pipelineeditor/WorkflowVisualEditor.java
@@ -1,7 +1,33 @@
 package org.jenkinsci.plugins.pipelineeditor;
 
+import hudson.Extension;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.kohsuke.stapler.DataBoundConstructor;
+
 /**
  * @author Kohsuke Kawaguchi
  */
-public class WorkflowVisualEditor {
+public class WorkflowVisualEditor extends CpsFlowDefinition {
+    /**
+     * JSON data model that the front end uses.
+     */
+    private final String json;
+
+    @DataBoundConstructor
+    public WorkflowVisualEditor(String script, String json) {
+        super(script);
+        this.json = json;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends CpsFlowDefinition.DescriptorImpl {
+        @Override
+        public String getDisplayName() {
+            return "Pipeline Visual Editor";
+        }
+    }
 }
