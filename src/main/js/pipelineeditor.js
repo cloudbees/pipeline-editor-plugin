2883fe6094f7c8346c47c2bab91837ead30d69ff
Michael Neale <michael.neale@gmail.com>
2015-11-09 15:02:56 +1100

    use bootstrap styles and a hash to remember to display editor

--- src/main/js/pipelineeditor.js
+++ src/main/js/pipelineeditor.js
@@ -9,20 +9,40 @@ $(document).ready(function () {
   
   window.mic = $; //For debugging!
   
+  if ("#pipeline-editor" === window.location.hash) {
+    showEditor($, confEditor, pageBody, script, json);
+  }
+  
   $('#edit-pipeline').click(function() {
-    confEditor.hide();
-    pageBody.append("<div id='pipeline-visual-editor'><div class='bootstrap-3'><input id='back-to-config' type=button value='OK yeah'></input><span class='glyphicon glyphicon-search' aria-hidden='true'></span></div></div>");
-    $('#back-to-config').click(function() {      
-      confEditor.show(); 
-      $("#pipeline-visual-editor").remove();     
-    });
+    showEditor($, confEditor, pageBody, script, json);
   });
- 
-  console.log(script);
-
-  console.log(json);
-  
-  
-
 
 });
+
+/**
+ * Clear the regular jenkins conf editor, show the pipeline editor
+ */ 
+function showEditor($, confEditor, pageBody, script, json) {
+  confEditor.hide();    
+  window.location.hash = "#pipeline-editor";
+  pageBody.append("<div id='pipeline-visual-editor'>" +
+                  bootstrap() + "<div class='bootstrap-3'>" +
+                  "<input id='back-to-config' type=button class='btn' value='Done'></input><span class='glyphicon glyphicon-search' aria-hidden='true'></span></div></div>");
+                  
+  $('#back-to-config').click(function() {      
+    confEditor.show();       
+    $("#pipeline-visual-editor").remove();     
+    window.location.hash = "";
+  });
+  
+  console.log(script);
+  console.log(json);
+
+}
+
+/**
+ * As we clear the page body, we can add our own bootstrap. 
+ */
+function bootstrap() {
+  return '<link id="pipeline-strapstyle" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" ></link>';
+}
