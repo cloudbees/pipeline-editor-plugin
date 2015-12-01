13bc067b024ec0c9c59a6aaaf7c570055b8b99cf
tfennelly <tom.fennelly@gmail.com>
2015-12-01 17:53:24 +0000

    Changed build to use bootstrap PHI plugin v1.2

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,6 @@ builder.src("src/main/js"); // remove quote when ready to face the pain
 builder.tests('src/test/js');
 
 builder.bundle('src/main/js/pipelineeditor.js')
-  .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3')
+  .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3', {addDefaultCSS: true})
   .inDir('src/main/resources/org/jenkinsci/plugins/pipelineeditor');
  
