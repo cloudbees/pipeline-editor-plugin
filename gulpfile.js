96b9dc316e22096e687dba79f77679760413d38d
tfennelly <tom.fennelly@gmail.com>
2015-12-02 19:49:34 +0000

    No need to define the src dirs

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,6 @@
 //
 var builder = require('jenkins-js-builder');
 
-
-builder.src("src/main/js"); // remove quote when ready to face the pain
-builder.tests('src/test/js');
-
 builder.bundle('src/main/js/pipelineeditor.js')
   .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3', {addDefaultCSS: true})
   .inDir('src/main/resources/org/jenkinsci/plugins/pipelineeditor');
