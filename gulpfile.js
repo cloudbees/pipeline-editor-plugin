7d0372da2d90d5c697665f17777d31a7e33d6f13
tfennelly <tom.fennelly@gmail.com>
2015-12-02 21:13:53 +0000

    Add handlebars hooks

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,6 @@ var builder = require('jenkins-js-builder');
 
 builder.bundle('src/main/js/pipelineeditor.js')
   .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3', {addDefaultCSS: true})
+  .withExternalModuleMapping('handlebars', 'handlebars:handlebars3')
   .inDir('src/main/resources/org/jenkinsci/plugins/pipelineeditor');
  
