e5ba44720f524f52aa79dae2b0cedb90ce252ccd
tfennelly <tom.fennelly@gmail.com>
2015-12-04 16:49:07 +0000

    Lets add some LESS/CSS

--- gulpfile.js
+++ gulpfile.js
@@ -6,5 +6,6 @@ var builder = require('jenkins-js-builder');
 builder.bundle('src/main/js/pipelineeditor.js')
   .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3', {addDefaultCSS: true})
   .withExternalModuleMapping('handlebars', 'handlebars:handlebars3')
+  .less('src/main/less/pipelineeditor.less')
   .inDir('src/main/resources/org/jenkinsci/plugins/pipelineeditor');
  
